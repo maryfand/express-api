a168ee68b5cc20bb884ea257adad0f03c51a2f17
maryfand <547284607@qq.com>
2016-10-07 16:29:14 +0800

    update

--- demo-mongoose-save-data/index.js
+++ demo-mongoose-save-data/index.js
@@ -2,19 +2,31 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 
+
 mongoose.connect('mongodb://localhost:27017/digicity-express-api');
-
-
-var Post = require('./models/post');
 
 var db = mongoose.connection;
 db.on('error', console.log);
 db.once('open', function() {
   console.log('success!')
 });
+var Schema = mongoose.Schema;
 
+var PostSchema = new Schema(
+  {
+    title: String,
+    content: String
+  }
+);
+
+var Post = mongoose.model('Post', PostSchema);
 app.post('/posts', function(req, res){
-  console.log('hello');
+  console.log('run post.save()');
+  var post = new Post({title:"myTitle",content:"mycontent"})
+  post.save(function(err){
+   if(err) return console.log(err);
+   console.log('saved');
+ });
 });
 
 app.listen(3000, function(){
