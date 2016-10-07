80269e85e7d9f5f3f21a91559242ef2b10873c66
maryfand <547284607@qq.com>
2016-10-07 15:05:11 +0800

    ”数据库连接“

---
+++ demo-mongoose-save-data/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/digicity-express-api');
+
+
+var Post = require('./models/post');
+
+var db = mongoose.connection;
+db.on('error', console.log);
+db.once('open', function() {
+  console.log('success!')
+});
+
+app.post('/posts', function(req, res){
+  console.log('hello');
+});
+
+app.listen(3000, function(){
+  console.log('running on port 3000.....');
+});
