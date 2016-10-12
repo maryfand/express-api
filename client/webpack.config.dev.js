46a6bee3ae5e8ba371df9a8f9a03e873001f9922
maryfand <547284607@qq.com>
2016-10-12 15:54:56 +0800

    erffe

--- client/webpack.config.dev.js
+++ client/webpack.config.dev.js
@@ -17,7 +17,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/dist/'
+    publicPath: '/static/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
